8b9e14d8e5d0fd02093512a9db06f43c47cb69dc
lyricalzy <lyricalzy@gmail.com>
2020-09-09 08:59:29 +0900

    jsp setting

--- demo/src/main/java/com/example/demo/test/controller/TestController.java
+++ demo/src/main/java/com/example/demo/test/controller/TestController.java
@@ -3,6 +3,10 @@ package com.example.demo.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -16,7 +20,21 @@ public class TestController {
     @ResponseBody
     @RequestMapping("/valueTest")
     public String valueTest(){
-        String value = "Test String";
-        return value;
+        String val = "Test String";
+        return val;
+    }
+
+    @RequestMapping("/test")
+    public ModelAndView test(){
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "lyricalzy");
+
+        List<String> testList = new ArrayList<>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+
+        mav.addObject("list", testList);
+        return mav;
     }
 }
