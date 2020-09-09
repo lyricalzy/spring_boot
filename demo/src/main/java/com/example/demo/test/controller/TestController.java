22eebddc24b91390ff17811a7c7bc5477f7fa125
lyricalzy <lyricalzy@gmail.com>
2020-09-09 12:07:06 +0900

    logback setting

--- demo/src/main/java/com/example/demo/test/controller/TestController.java
+++ demo/src/main/java/com/example/demo/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Controller
 public class TestController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/home")
     public String home() {
@@ -35,6 +39,13 @@ public class TestController {
         testList.add("c");
 
         mav.addObject("list", testList);
+
+        logger.trace("Trace Level Test");
+        logger.debug("DEBUG Level Test");
+        logger.info("INFO Level Test");
+        logger.warn("WARN Level Test");
+        logger.error("ERROR Level Test");
+
         return mav;
     }
 }
